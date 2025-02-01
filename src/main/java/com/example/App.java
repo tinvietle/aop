67c8a08e0d9d8ffec22132912709f615a8717a10
faugeu <10422050@student.vgu.edu.vn>
2025-02-01 12:50:30 +0100

    Split App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -50,9 +50,9 @@ public class App extends Application {
         return fxmlLoader.load();
     }
 
-    public static void main(String[] args) {
-        launch();
-    }
+    // public static void main(String[] args) {
+    //     launch();
+    // }
 
     public void closeProgram() {
         if (Utils.confirmExit()) {
---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class Main {
+    public static void main(final String[] args) {
+        App.launch(App.class, args);
+    }
+}
